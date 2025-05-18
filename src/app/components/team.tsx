9a42a6eaac8b11b6755a558344334f5eb018604b
Bettersf <166757622+Bettersf@users.noreply.github.com>
2025-05-18 14:00:25 +0200

    Update team.tsx

--- src/app/components/team.tsx
+++ src/app/components/team.tsx
@@ -24,19 +24,19 @@ const team = [
 export default function Team() {
   return (
     <section className="w-full flex flex-col items-center gap-8 mt-16">
-      <h2 className="text-[38px] text-border-White text_shadow">MEET THE TEAM</h2>
+      <h2 className="text-[38px] text-border-White font-bold text_shadow">MEET THE TEAM</h2>
       <div className="flex flex-col md:flex-row gap-10 justify-center w-full px-4">
         {team.map((member) => (
           <div key={member.name} className="flex flex-col items-center gap-4 bg-black bg-opacity-40 backdrop-blur-md rounded-xl p-4 box_shadow">
             <div className="relative dashoboard__teamimg w-full">
-              <img src={member.img} alt={member.name} className="border-[3px] border-akatsukiRed rounded-md w-full" />
-              <p className="absolute bottom-[-1rem] left-1/2 transform -translate-x-1/2 text-white text-content bg-bgColor border border-akatsukiRed px-3 py-1 rounded-md box_shadow">
+              <img src={member.img} alt={member.name} className="border-[6px] border-akatsukiRed rounded-md w-full" />
+              <p className="absolute bottom-[-1rem] left-1/2 transform -translate-x-1/2 text-white font-bold text-content bg-bgColor border border-akatsukiRed px-3 py-1 rounded-md box_shadow">
                 {member.name}
               </p>
             </div>
             <Link
               href={member.twitter}
-              className="text-borderYellow border border-border-akatsukiRed rounded-lg px-4 py-2 hover:bg-[#907848] hover:text-[#132a3b] text-center flex items-center gap-2"
+              className="text-borderYellow border border-border-akatsukiRed font-bold rounded-lg px-4 py-2 hover:bg-[#907848] hover:text-[#132a3b] text-center flex items-center gap-2"
               target="_blank"
             >
               <FaXTwitter /> FOLLOW {member.name.toUpperCase()}
